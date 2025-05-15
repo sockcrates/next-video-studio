95334016452fb61c92e2283ce2c73e33ef96ab86
markg <17348049+sockcrates@users.noreply.github.com>
2025-05-15 14:01:10 -0400

    Fix class bug

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import classNames from "classnames";
 import React, { type ComponentPropsWithoutRef } from "react";
 
 export function Button({
@@ -9,7 +10,10 @@ export function Button({
 }: ComponentPropsWithoutRef<"button">) {
   return (
     <button
-      className={`rounded-md bg-purple-500 px-4 py-2 text-white hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50 disabled:bg-gray-800 disabled:cursor-not-allowed ${className}`}
+      className={classNames(
+        "rounded-md bg-purple-500 px-4 py-2 text-white hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50 disabled:bg-gray-800 disabled:cursor-not-allowed",
+        className,
+      )}
       {...props}
     >
       {children}
