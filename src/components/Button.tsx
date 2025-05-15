f94f962a6eda953fd88af40c2d9a0c52dcde9e89
markg <17348049+sockcrates@users.noreply.github.com>
2025-05-15 16:21:32 -0400

    Fix light mode disabled button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export const Button = memo(
   ({ children, className, ...props }: ComponentPropsWithoutRef<"button">) => (
     <button
       className={classNames(
-        "rounded-md bg-purple-500 px-4 py-2 text-white hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50 disabled:bg-gray-800 disabled:cursor-not-allowed",
+        "rounded-md bg-purple-500 px-4 py-2 text-white hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-opacity-50 disabled:bg-gray-400 dark:disabled:bg-gray-800 disabled:cursor-not-allowed",
         className,
       )}
       {...props}
